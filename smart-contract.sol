3673c092b0dc5b5bf5bcb62d343d7fe10bfb239c
İsmet AKPINAR <104082595+ismetakpinar@users.noreply.github.com>
2022-12-29 15:55:41 +0300

    Update smart-contract.sol

--- smart-contract.sol
+++ smart-contract.sol
@@ -1,22 +1,22 @@
-pragma solidity ^0.8.7;
+// SPDX-License-Identifier: GPL-3.0
 
-contract HelloWorld {
-    enum LANGUAGE {EN, FR, DE}
-    
-    string english = "Hello World";
-    string french  = "Bonjour Le Monde";
-    string german  = "Hallo Welt";
-    
-    function HelloWorld() public {
+pragma solidity >=0.7.0 <0.9.0;
+contract FeeCollector {
+    address public owner;
+    uint256 public balance;
+
+    constructor() {
+        owner = msg.sender; //0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
     }
-    
-    function sayHello(LANGUAGE lang) public view returns(string) {
-        if (lang == LANGUAGE.EN) {
-            return english;
-        } else if (lang == LANGUAGE.FR) {
-            return french;
-        } else {
-            return german;
-        }
+
+    receive() external payable {
+        balance += msg.value;
+    }
+
+    function withdraw(uint256 amount, address payable destAddr) public {
+        require(msg.sender == owner, "Only owner can withdraw");
+        require(amount <= balance, "Insufficient funds");
+        destAddr.transfer(amount);
+        balance -= amount;
     }
 }
